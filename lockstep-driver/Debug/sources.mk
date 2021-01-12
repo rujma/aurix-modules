2573c5ceafd85e3bfed8f10b159848efa00e6688
Rui Almeida <rjmalmeida0@gmail.com>
2021-01-12 19:05:35 +0000

    project init

---
+++ lockstep-driver/Debug/sources.mk
@@ -0,0 +1,92 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+C_UPPER_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+OBJ_SRCS := 
+CC_SRCS := 
+SRC_SRCS := 
+CPP_SRCS := 
+C_SRCS := 
+O_SRCS := 
+CC_DEPS := 
+C++_DEPS := 
+EXECUTABLES := 
+C_UPPER_DEPS := 
+CXX_DEPS := 
+OBJS := 
+SECONDARY_SIZE := 
+COMPILED_SRCS := 
+CPP_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Configurations/Debug \
+. \
+Libraries/Infra/Platform/Tricore/Compilers \
+Libraries/Service/CpuGeneric/If \
+Libraries/Service/CpuGeneric/StdIf \
+Libraries/Service/CpuGeneric/SysSe/Bsp \
+Libraries/Service/CpuGeneric/SysSe/Comm \
+Libraries/Service/CpuGeneric/SysSe/General \
+Libraries/Service/CpuGeneric/SysSe/Math \
+Libraries/Service/CpuGeneric/SysSe/Time \
+Libraries/iLLD/TC23A/Tricore/Asclin/Asc \
+Libraries/iLLD/TC23A/Tricore/Asclin/Lin \
+Libraries/iLLD/TC23A/Tricore/Asclin/Spi \
+Libraries/iLLD/TC23A/Tricore/Asclin/Std \
+Libraries/iLLD/TC23A/Tricore/Ccu6/Icu \
+Libraries/iLLD/TC23A/Tricore/Ccu6/PwmBc \
+Libraries/iLLD/TC23A/Tricore/Ccu6/PwmHl \
+Libraries/iLLD/TC23A/Tricore/Ccu6/Std \
+Libraries/iLLD/TC23A/Tricore/Ccu6/TPwm \
+Libraries/iLLD/TC23A/Tricore/Ccu6/Timer \
+Libraries/iLLD/TC23A/Tricore/Ccu6/TimerWithTrigger \
+Libraries/iLLD/TC23A/Tricore/Cpu/CStart \
+Libraries/iLLD/TC23A/Tricore/Cpu/Irq \
+Libraries/iLLD/TC23A/Tricore/Cpu/Std \
+Libraries/iLLD/TC23A/Tricore/Cpu/Trap \
+Libraries/iLLD/TC23A/Tricore/Dma/Dma \
+Libraries/iLLD/TC23A/Tricore/Dma/Std \
+Libraries/iLLD/TC23A/Tricore/Dts/Dts \
+Libraries/iLLD/TC23A/Tricore/Dts/Std \
+Libraries/iLLD/TC23A/Tricore/Eray/Eray \
+Libraries/iLLD/TC23A/Tricore/Eray/Std \
+Libraries/iLLD/TC23A/Tricore/Eth/Phy_Pef7071 \
+Libraries/iLLD/TC23A/Tricore/Eth/Std \
+Libraries/iLLD/TC23A/Tricore/Flash/Std \
+Libraries/iLLD/TC23A/Tricore/Gpt12/IncrEnc \
+Libraries/iLLD/TC23A/Tricore/Gpt12/Std \
+Libraries/iLLD/TC23A/Tricore/Gtm/Std \
+Libraries/iLLD/TC23A/Tricore/Gtm/Tim/In \
+Libraries/iLLD/TC23A/Tricore/Gtm/Tom/Pwm \
+Libraries/iLLD/TC23A/Tricore/Gtm/Tom/PwmHl \
+Libraries/iLLD/TC23A/Tricore/Gtm/Tom/Timer \
+Libraries/iLLD/TC23A/Tricore/Gtm/Trig \
+Libraries/iLLD/TC23A/Tricore/Iom/Driver \
+Libraries/iLLD/TC23A/Tricore/Iom/Std \
+Libraries/iLLD/TC23A/Tricore/Mtu/Std \
+Libraries/iLLD/TC23A/Tricore/Multican/Can \
+Libraries/iLLD/TC23A/Tricore/Multican/Std \
+Libraries/iLLD/TC23A/Tricore/Port/Io \
+Libraries/iLLD/TC23A/Tricore/Port/Std \
+Libraries/iLLD/TC23A/Tricore/Qspi/SpiMaster \
+Libraries/iLLD/TC23A/Tricore/Qspi/SpiSlave \
+Libraries/iLLD/TC23A/Tricore/Qspi/Std \
+Libraries/iLLD/TC23A/Tricore/Scu/Std \
+Libraries/iLLD/TC23A/Tricore/Sent/Sent \
+Libraries/iLLD/TC23A/Tricore/Sent/Std \
+Libraries/iLLD/TC23A/Tricore/Smu/Std \
+Libraries/iLLD/TC23A/Tricore/Src/Std \
+Libraries/iLLD/TC23A/Tricore/Stm/Std \
+Libraries/iLLD/TC23A/Tricore/Stm/Timer \
+Libraries/iLLD/TC23A/Tricore/Vadc/Adc \
+Libraries/iLLD/TC23A/Tricore/Vadc/Std \
+Libraries/iLLD/TC23A/Tricore/_Impl \
+Libraries/iLLD/TC23A/Tricore/_Lib/DataHandling \
+Libraries/iLLD/TC23A/Tricore/_Lib/InternalMux \
+Libraries/iLLD/TC23A/Tricore/_PinMap \
+
